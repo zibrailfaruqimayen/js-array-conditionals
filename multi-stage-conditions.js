385d85cbd8bf4403394ba8cb9d4ad1be30e848b8
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-27 23:09:14 +0600

    js-multiple-conditions

---
+++ multi-stage-conditions.js
@@ -0,0 +1,15 @@
+var money = 68;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+
+if(danishPrice < money) {
+    console.log('dan dan danish khamu');
+}
+ else if (butterBread < money) {
+    console.log('butter bon khamu');
+ }
+ else if (toastBiscuit < money){
+    console.log('chubai chubai toast biscuit khamu')
+ }
