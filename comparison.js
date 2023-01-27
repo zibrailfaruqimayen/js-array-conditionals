5ddf28f76e19c33d015db77e7ae34d85b76dc0d1
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-27 21:19:21 +0600

    js-comparison

---
+++ comparison.js
@@ -0,0 +1,26 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(5 != 6);
+// console.log(15 != 15);
+//less than or equal
+
+// console.log(5 <= 6);
+// console.log(6 <= 6);
+// console.log(16 <= 6);
+
+//greater then or equal
+// console.log(5 >= 6);
+// console.log(5 >= 2);
+// console.log(2 >= 2);
+
+var num1 = 56;
+var num2 = 89;
+
+console.log( num1 < num2);
+console.log( num1 > num2);
+console.log( num1 === num2);
+console.log( num1 !== num2);
+console.log( num1 >= num2);
+console.log( num1 <= num2);
