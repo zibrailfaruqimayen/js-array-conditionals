6dd9b694077958eb64eab37828a13931d4bb24ff
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-27 22:33:01 +0600

    js-multiple-conditions

---
+++ multiple-conditions.js
@@ -0,0 +1,43 @@
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 1;
+
+// if(isGraduated == true){
+//     console.log('esho biye kore feli');
+// }
+// else{
+//     console.log('tor kopale biya nai');
+// }
+
+
+// if(isGraduated == true && salary > 50000){
+//     console.log('esho biye kore feli');
+// }
+// else{
+//     console.log('tor kopale biya nai');
+// }
+
+
+// if(isGraduated == true && salary > 50000 && cars >= 1){
+//     console.log('esho biye kore feli');
+// }
+// else{
+//     console.log('tor kopale biya nai');
+// }
+
+
+var isGraduated = true;
+var salary = 75000;
+var cars = 1;
+
+
+if(isGraduated === true || salary > 50000 || cars >=1){
+    console.log('eso pram kori');
+}
+else{
+    console.log('tomar friend er mobile number dao')
+}
+
+if (isGraduated === true && (salary > 50000 || cars > 1)){
+
+}
