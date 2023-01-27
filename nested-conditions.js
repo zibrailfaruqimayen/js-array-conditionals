484afcbc058a97dd3c4cbe6c70d1e90a9ebbf7af
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-27 23:43:48 +0600

    js-multiple-conditions

---
+++ nested-conditions.js
@@ -0,0 +1,18 @@
+var math = true;
+var geometry = true;
+var straightLint =true;
+
+if(math ==true){
+    if(geometry == true){
+        if(straightLint == true){
+
+        }
+        else{
+            console.log('baka pothe cholba na')
+        }
+    }
+    else{
+        console.log('pithagorous hoite parba na')
+    }
+}
+
