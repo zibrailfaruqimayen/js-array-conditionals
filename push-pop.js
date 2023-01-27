d76a5eae6b5a521790dbdf46a3956d41d60ddcf3
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-27 20:28:57 +0600

    js-comparison

---
+++ push-pop.js
@@ -0,0 +1,18 @@
+var numbers = [78, 45, 98, 45];
+
+//use push to add element to an array as the last element array 
+// numbers.push(66);
+// console.log(numbers);
+
+var friends = ['balam', 'kalam', 'salam'];
+friends.push('gelam');
+friends.push('pailam');
+
+// console.log(friends);
+
+//use pop to get last element (out of line)
+console.log(numbers);
+//numbers.pop();
+var element = numbers.pop();
+numbers.pop();
+console.log(numbers)
