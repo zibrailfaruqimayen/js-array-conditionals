4e64c8774c7460762ad827678bb3a09bc7ddd5e6
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-27 16:33:19 +0600

    js-recap

---
+++ variable.js
@@ -0,0 +1,29 @@
+// number type variable
+var sunglassPrice = 120;
+
+// string type variable
+var specialOneName = "Ariana Grande";
+
+// boolean type variable
+var isSingle = true;
+
+
+// // tow types of number: integer: 120 (purno songkha)
+// // float, decimal: 3.65 (vognanso)
+
+// // variable naming convention: rules, best practice
+// /**
+//  * 1. single word
+//  * 2. no gap or space
+//  * 3. no quote
+//  * 4. no keyword
+//  * 5. can not start with a number 
+//  * 6. can not contain dash
+//  * 7. should use camelCase
+//  * /
+ 
+// // math Operation: +, -, *, / 
+// // shorthands: +=, -=, ++, -- 
+// // convert: parseInt, parseFloat 
+// // toFixed
+// // remainder: vagses: %  
