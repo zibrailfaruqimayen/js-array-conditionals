9080a04a61aed881f197721f78ca1653df8a515a
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-27 22:05:03 +0600

    js-comparison

---
+++ conditional.js
@@ -0,0 +1,26 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+// if (condition) {
+//     will execute if the condition is true
+// }
+
+if (iphonePrice < myBudget){
+    console.log('iphone kine futani marbo');
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+// if (chickenPrice <= myMoney){
+//     console.log('murgir raan khamu and haddi chibamu!!!');
+// }
+// if (chikenPrice > myMoney){
+//     console.log('Smashed potato and lentils soup');
+// }
+
+
+if(chickenPrice < myMoney) {
+    console.log('I will eat chicken');
+}
+else{
+    console.log('I will eat potato');
+}
